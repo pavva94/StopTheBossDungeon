78cec0234aee0a97183c3661f3e055b98f92716a
Alessandro Pavesi <pavesialessandro@live.it>
2016-02-14 18:03:11 +0100

    Inserito Touch come modalità principale

--- SuperSpartyBros/Assets/Scripts/CharacterController2D.cs
+++ SuperSpartyBros/Assets/Scripts/CharacterController2D.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class CharacterController2D : MonoBehaviour {
 
@@ -88,7 +89,7 @@ public class CharacterController2D : MonoBehaviour {
 			return;
 
 		// determine horizontal velocity change based on the horizontal input
-		_vx = Input.GetAxisRaw ("Horizontal");
+		_vx = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
 
 		// Determine if running based on the horizontal movement
 		if (_vx != 0) 
@@ -118,10 +119,10 @@ public class CharacterController2D : MonoBehaviour {
 		// Set the grounded animation states
 		_animator.SetBool("Grounded", _isGrounded);
 
-		if(_isGrounded && Input.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
+		if(_isGrounded && CrossPlatformInputManager.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
 		{
             DoJump();
-		} else if (_canDoubleJump && Input.GetButtonDown("Jump")) // if can doble jump and jump button pressed, it can double jump
+		} else if (_canDoubleJump && CrossPlatformInputManager.GetButtonDown("Jump")) // if can doble jump and jump button pressed, it can double jump
         {
             DoJump();
             // disable double jump after double jumping
@@ -130,7 +131,7 @@ public class CharacterController2D : MonoBehaviour {
 	
 		// If the player stops jumping mid jump and player is not yet falling
 		// then set the vertical velocity to 0 (he will start to fall from gravity)
-		if(Input.GetButtonUp("Jump") && _vy>0f)
+		if(CrossPlatformInputManager.GetButtonUp("Jump") && _vy>0f)
 		{
 			_vy = 0f;
 		}
